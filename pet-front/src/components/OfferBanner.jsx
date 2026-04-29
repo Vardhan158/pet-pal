472cac1a233a0e1164cba086b3302b28baaa2ff6
Vardhan158 <harshavardhandevang@gmail.com>
2026-04-29 12:32:16 +0530

    Update OfferBanner.jsx

--- pet-front/src/components/OfferBanner.jsx
+++ pet-front/src/components/OfferBanner.jsx
@@ -9,7 +9,7 @@ export default function OfferBanner() {
   // 🧠 Fetch active offer from backend
   const fetchActiveOffer = async () => {
     try {
-      const { data } = await axios.get("http://localhost:5008/api/offers");
+      const { data } = await axios.get("https://pet-pal-x74f.onrender.com/api/offers");
       if (data.success && data.offer) {
         setOffer(data.offer);
       } else {
