139d78337da5296d1499190768545c86580d80ee
Vardhan158 <harshavardhandevang@gmail.com>
2026-04-29 12:31:10 +0530

    Update axiosInstance.js

--- pet-front/src/api/utils/axiosInstance.js
+++ pet-front/src/api/utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5008/api",
+  baseURL: "https://pet-pal-x74f.onrender.com/api",
   withCredentials: false,
 });
 
